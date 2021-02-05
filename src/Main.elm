3152260a1e6659fe0d3d5f05b35967c906a26efd
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-02-05 06:57:59 +0100

    Switch to Dict model

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser exposing (Document, UrlRequest)
 import Browser.Navigation as Nav
+import Dict exposing (Dict)
 import Element exposing (Element, rgb)
 import Element.Background
 import Element.Border as Border
@@ -9,10 +10,19 @@ import Element.Font as Font
 import Html.Attributes
 import Palette
 import Url exposing (Url)
+import Url.Parser as UrlP exposing ((</>))
 
 
-type Model
-    = Model Lesson
+type alias Model =
+    { navKey : Nav.Key
+    , page : Page
+    , lessonLookup : Dict String Lesson
+    }
+
+
+type Page
+    = LessonId String
+    | LandingPage
 
 
 type alias Lesson =
@@ -38,13 +48,12 @@ type Msg
     | ClickLink UrlRequest
 
 
-init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url navKey =
-    ( Model
-        { title =
-            "Using TDD to write a Leap Years function"
-        , summary =
-            """
+tddLesson : Lesson
+tddLesson =
+    { title =
+        "Using TDD to write a Leap Years function"
+    , summary =
+        """
 This is often the first exercise I do with new teams.
 You have to TDD a function that takes an integer argument
 and returns a boolean. It ends up being a rather small piece
@@ -57,61 +66,106 @@ modulo operator when implementing the first test case. That
 keeps the demo shorter, and means you don’t need to explain
 triangulation as a concept.
 """
-        , sections =
-            [ { header = "Session Outline"
-              , body =
-                    [ BulletList
-                        [ "10 min connect: divide into pairs, write down 3 benefits of TDD"
-                        , "15 min concept: demo leap years"
-                        , "20 min do: leap years in pairs"
-                        , "10 min reflect: summary of main idea"
-                        ]
+    , sections =
+        [ { header = "Session Outline"
+          , body =
+                [ BulletList
+                    [ "10 min connect: divide into pairs, write down 3 benefits of TDD"
+                    , "15 min concept: demo leap years"
+                    , "20 min do: leap years in pairs"
+                    , "10 min reflect: summary of main idea"
                     ]
-              }
-            , { header = "Connect"
-              , body =
-                    [ Paragraph """
+                ]
+          }
+        , { header = "Connect"
+          , body =
+                [ Paragraph """
 Have everyone stand up and
 stand at one side of the room.
 Ask them to walk a few steps towards the other side of the
 room in proportion to their answers to these questions. More
 confidence means they should walk further:"""
-                    , BulletList
-                        [ "confidence pair programming"
-                        , "confidence with unit testing"
-                        , "confidence with Test-Driven Development"
-                        ]
+                , BulletList
+                    [ "confidence pair programming"
+                    , "confidence with unit testing"
+                    , "confidence with Test-Driven Development"
                     ]
-              }
-            ]
-        }
+                ]
+          }
+        ]
+    }
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url navKey =
+    ( { navKey = navKey
+      , page = LandingPage
+      , lessonLookup = Dict.fromList [ ( "tdd-introduction", tddLesson ) ]
+      }
     , Cmd.none
     )
 
 
-view : Model -> Document msg
-view model =
-    let
-        ideaPadLink =
-            Element.link
-                [ Element.alignBottom
-                , Element.alignRight
-                , Element.htmlAttribute (Html.Attributes.target "_blank")
-                ]
-                { url = "https://docs.google.com/document/d/17aUn98u1xRURT7ifPApN633IDSlXLqJMecwCh3Z_fok/edit"
-                , label = Element.text "Idea Scratch Pad"
-                }
-    in
-    { title = "Learning Hour Kit"
-    , body =
-        [ Element.layout
-            [ Element.Background.color Palette.pageBackground
-            , Element.inFront ideaPadLink
-            ]
-            -- layout converts from Element to Html
-            (lessonBox model)
+lessonRoute : UrlP.Parser (Page -> a) a
+lessonRoute =
+    UrlP.map LessonId (UrlP.s "lesson" </> UrlP.string)
+
+
+parseUrl : String -> Page
+parseUrl string =
+    case Url.fromString string of
+        Nothing ->
+            LandingPage
+
+        Just url ->
+            Maybe.withDefault LandingPage (UrlP.parse lessonRoute url)
+
+
+ideaPadLink =
+    Element.link
+        [ Element.alignBottom
+        , Element.alignRight
+        , Element.htmlAttribute (Html.Attributes.target "_blank")
         ]
-    }
+        { url = "https://docs.google.com/document/d/17aUn98u1xRURT7ifPApN633IDSlXLqJMecwCh3Z_fok/edit"
+        , label = Element.text "Idea Scratch Pad"
+        }
+
+
+view : Model -> Document msg
+view { navKey, page, lessonLookup } =
+    case page of
+        LandingPage ->
+            { title = "Olof's Learning Hour Kit"
+            , body =
+                [ Element.layout
+                    [ Element.Background.color Palette.pageBackground
+                    , Element.inFront ideaPadLink
+                    ]
+                    (Element.text "LANDING PAGE")
+                ]
+            }
+
+        LessonId lessonId ->
+            let
+                lesson =
+                    case Dict.get lessonId lessonLookup of
+                        Just data ->
+                            data
+
+                        Nothing ->
+                            Debug.todo "better modelling"
+            in
+            { title = "Learning Hour Kit"
+            , body =
+                [ Element.layout
+                    [ Element.Background.color Palette.pageBackground
+                    , Element.inFront ideaPadLink
+                    ]
+                    -- layout converts from Element to Html
+                    (lessonBox lesson)
+                ]
+            }
 
 
 paraE : String -> Element a
@@ -132,8 +186,8 @@ headerE text =
         (Element.text text)
 
 
-lessonBox : Model -> Element a
-lessonBox (Model lesson) =
+lessonBox : Lesson -> Element a
+lessonBox lesson =
     let
         summaryE =
             paraE lesson.summary
